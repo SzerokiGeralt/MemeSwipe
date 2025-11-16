9fefa8ccbb7caa4412acf911f5971c3b993b3b01
Karol <77929428+SzerokiGeralt@users.noreply.github.com>
2025-11-16 13:45:13 +0100

    Profile page

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -3,7 +3,14 @@ require_once 'AppController.php';
 
 class ProfileController extends AppController {
     public function show(?string $id) {
-        $user = ['id' => $id];
+        if ($id === null || !is_numeric($id)) {
+            $id = '1';
+        }
+        $user = ['id' => $id, 
+                'imageUrlPath' => 'https://randomuser.me/api/portraits/men/' . $id . '.jpg', 
+                'nickname' => 'User' . $id,
+                'level' => 5 + (int)$id,
+                'streak' => 3 + (int)$id];
         return $this->render('profile', ['user' => $user]);
     }
 }
