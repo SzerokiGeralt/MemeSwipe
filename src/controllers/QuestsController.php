350c878fae5c1403261517b86282362523a3fee6
SzerokiGeralt <szerokigeralt@gmail.com>
2026-01-25 23:58:34 +0100

    Quest/store mockup

--- src/controllers/QuestsController.php
+++ src/controllers/QuestsController.php
@@ -1,14 +1,26 @@
 <?php
 require_once 'AppController.php';
+require_once __DIR__ . '/../repository/UserRepository.php';
 
 class QuestsController extends AppController {
+    private $userRepository;
+
+    public function __construct() {
+        $this->userRepository = new UserRepository();
+    }
+
     public function index(?string $id) {
         if (!isset($_SESSION['user_id'])) {
-                // Not logged in - redirect to login page
-                header('Location: /login');
-                exit();
-            }
-        $user = ['id' => $id];
-        return $this->render('quests', ['user' => $user]);
+            // Not logged in - redirect to login page
+            header('Location: /login');
+            exit();
+        }
+
+        // Get current user stats for navbar
+        $userStats = $this->userRepository->getUserById($_SESSION['user_id']);
+
+        return $this->render('quests', [
+            'stats' => $userStats
+        ]);
     }
 }
